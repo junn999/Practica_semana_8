c6dc9412d5bb03e9a8ea20d0290db6b488279594
DarlinRamos <junioralberto1421@gmail.com>
2022-09-23 23:15:01 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Ejercicio_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMensaje1 = new System.Windows.Forms.Button();
+            this.txtMensaje1 = new System.Windows.Forms.TextBox();
+            this.txtMensaje2 = new System.Windows.Forms.TextBox();
+            this.btnMensaje2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMensaje1
+            // 
+            this.btnMensaje1.Location = new System.Drawing.Point(33, 82);
+            this.btnMensaje1.Name = "btnMensaje1";
+            this.btnMensaje1.Size = new System.Drawing.Size(176, 23);
+            this.btnMensaje1.TabIndex = 0;
+            this.btnMensaje1.Text = "Enviar mensaje 1";
+            this.btnMensaje1.UseVisualStyleBackColor = true;
+            this.btnMensaje1.Click += new System.EventHandler(this.btnMensaje1_Click);
+            // 
+            // txtMensaje1
+            // 
+            this.txtMensaje1.Location = new System.Drawing.Point(33, 40);
+            this.txtMensaje1.Name = "txtMensaje1";
+            this.txtMensaje1.Size = new System.Drawing.Size(176, 20);
+            this.txtMensaje1.TabIndex = 1;
+            // 
+            // txtMensaje2
+            // 
+            this.txtMensaje2.Location = new System.Drawing.Point(302, 40);
+            this.txtMensaje2.Name = "txtMensaje2";
+            this.txtMensaje2.Size = new System.Drawing.Size(176, 20);
+            this.txtMensaje2.TabIndex = 3;
+            // 
+            // btnMensaje2
+            // 
+            this.btnMensaje2.Location = new System.Drawing.Point(302, 82);
+            this.btnMensaje2.Name = "btnMensaje2";
+            this.btnMensaje2.Size = new System.Drawing.Size(176, 23);
+            this.btnMensaje2.TabIndex = 2;
+            this.btnMensaje2.Text = "Enviar mensaje 2";
+            this.btnMensaje2.UseVisualStyleBackColor = true;
+            this.btnMensaje2.Click += new System.EventHandler(this.btnMensaje2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 159);
+            this.Controls.Add(this.txtMensaje2);
+            this.Controls.Add(this.btnMensaje2);
+            this.Controls.Add(this.txtMensaje1);
+            this.Controls.Add(this.btnMensaje1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMensaje1;
+        private System.Windows.Forms.TextBox txtMensaje1;
+        private System.Windows.Forms.TextBox txtMensaje2;
+        private System.Windows.Forms.Button btnMensaje2;
+    }
+}
+
---
+++ Ejercicio_2/Form2.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Ejercicio_2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Nombres";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(258, 330);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(107, 23);
+            this.btnClose.TabIndex = 15;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(119, 330);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(107, 23);
+            this.btnCargar.TabIndex = 14;
+            this.btnCargar.Text = "Cargar datos";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(146, 110);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(219, 20);
+            this.txtApellidos.TabIndex = 13;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(146, 73);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(219, 20);
+            this.txtNombres.TabIndex = 12;
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(146, 30);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(219, 20);
+            this.txtId.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Apellidos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Id";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(300, 150);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
